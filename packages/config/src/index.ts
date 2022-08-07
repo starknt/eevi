b03c0fa824e250279203844f83944fa593f92ff2
starknt <1431880400@qq.com>
2022-08-07 22:32:00 +0800

    feat: config package, resolve config and load config for eevi.config.ts/js or user inline config

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,8 +1,10 @@
 import fs from 'fs'
-import { dirname, resolve } from 'path'
+import { dirname, isAbsolute, join, resolve } from 'path'
+import { builtinModules } from 'module'
 import { createConfigLoader } from 'unconfig'
 import type { LoadConfigResult } from 'unconfig'
-import type { UserConfig, UserConfigExport } from '@eevi/core'
+import type { ResolvedConfig, UserConfig, UserConfigExport } from '@eevi/core'
+import type { ResolvedConfig as ViteUserConfig } from 'vite'
 
 export async function loadConfig<U extends UserConfig>(cwd = process.cwd(), configOrPath: string | U = cwd): Promise<LoadConfigResult<U>> {
   let inlineConfig = {} as U
@@ -45,13 +47,32 @@ export async function loadConfig<U extends UserConfig>(cwd = process.cwd(), conf
   return result
 }
 
-export function resolveConfig<T extends UserConfig>(config: T): T {
-  return {
-    ...config,
-    base: config.base ?? '/',
-    root: config.root ?? 'app/electron',
-    entry: config.entry ?? 'app/electron/main.ts',
-  }
+export function resolveConfig(config: UserConfig, viteConfig: ViteUserConfig): ResolvedConfig {
+  const resolvedConfig = {} as ResolvedConfig
+
+  resolvedConfig.base = config.base ?? viteConfig.base
+  resolvedConfig.root = config.root ?? viteConfig.root
+  resolvedConfig.entry = isAbsolute(config.entry) ? config.entry : join(resolvedConfig.base, config.entry)
+  resolvedConfig.preloadEntries = config.preloadEntries ?? []
+  resolvedConfig.preloadEntries = resolvedConfig.preloadEntries.map((entry) => {
+    if (!isAbsolute(entry))
+      return join(resolvedConfig.root, entry)
+
+    return entry
+  })
+  resolvedConfig.minify = config.minify ?? viteConfig.mode === 'production'
+  resolvedConfig.external = ['electron', ...builtinModules, ...(config.external ?? [])]
+  resolvedConfig.inject = [...(config.inject ?? [])]
+  resolvedConfig.outdir = isAbsolute(config.outDir) ? config.outDir : resolve(resolvedConfig.base, config.outDir)
+  resolvedConfig.mode = config.mode ?? process.env.NODE_ENV as any
+  resolvedConfig.plugins = config.plugin ?? []
+  resolvedConfig.sourcemap = config.sourcemap ?? process.env.NODE_ENV !== 'production'
+  resolvedConfig.resolve = config.resolve
+  resolvedConfig.tsconfig = config.tsconfig ? isAbsolute(config.tsconfig) ? config.tsconfig : resolve(resolvedConfig.base, resolvedConfig.root, 'tsconfig.json') : join(resolvedConfig.base, resolvedConfig.root, 'tsconfig.json')
+  resolvedConfig.define = config.define ?? {}
+  resolvedConfig.debounceMs = config.debounceMs ?? 1000 * 2
+
+  return resolvedConfig
 }
 
 export function defineConfig(config: UserConfigExport): UserConfigExport {
