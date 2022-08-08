3c73882f8150c2c1a95463975187d77a84b403dd
starknt <1431880400@qq.com>
2022-08-08 13:44:44 +0800

    chore: esbuild is plugin test

--- playground/eevi.config.ts
+++ playground/eevi.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'eevi'
+import { esbuildIsPlugin } from 'eevi/esbuild'
 
 export default defineConfig({
   root: 'src-electron',
   entry: 'src-electron/main.ts',
   preloadEntries: ['src-electron/preload/common.ts'],
   outDir: 'dist',
+  plugin: [esbuildIsPlugin()],
 })
