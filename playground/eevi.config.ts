d1cafabb8df492bbb09f61d41591320798315b43
starknt <1431880400@qq.com>
2022-08-08 10:37:21 +0800

    chore: config test

--- playground/eevi.config.ts
+++ playground/eevi.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'eevi'
 
 export default defineConfig({
-  base: './',
   root: 'src-electron',
   entry: 'src-electron/main.ts',
   preloadEntries: ['src-electron/preload/common.ts'],
