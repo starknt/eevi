5176f08c2f912ace84cc474e3da291d5c21d42e9
starknt <1431880400@qq.com>
2022-08-19 21:05:51 +0800

    chore: inspect plugin

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Inspect from 'vite-plugin-inspect'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    Inspect(),
+  ],
 })
