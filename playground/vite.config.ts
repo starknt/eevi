a673b87aa7051db72346a8e9b9630464d1e9654f
starknt <1431880400@qq.com>
2022-08-22 16:52:52 +0800

    chore: playground

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Inspect from 'vite-plugin-inspect'
+import Eevi, { EeviMpaPlugin } from 'eevi/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     Inspect(),
+    Eevi.EeviMpaPlugin({
+      template: './index.html',
+      pages: [
+        {
+          name: 'main',
+          entry: './src/main.ts',
+        },
+        {
+          name: 'other',
+          entry: './src/main.ts',
+        },
+      ],
+    }),
   ],
+  clearScreen: false,
 })
