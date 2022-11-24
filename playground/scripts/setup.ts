e5ff338afab05a6bb5968bcd6acdd1bae9347cf4
starknt <1431880400@qq.com>
2022-11-25 02:40:29 +0800

    chore: lint

--- playground/scripts/setup.ts
+++ playground/scripts/setup.ts
@@ -21,5 +21,4 @@ if (!fs.existsSync(srcElectronModulesPath) && fs.existsSync(appModulesPath))
 if (!fs.existsSync(srcElectronPackagePath) && fs.existsSync(appPackagePath))
   tasks.push(fsPromises.symlink(appPackagePath, srcElectronPackagePath, 'file'))
 
-
 Promise.allSettled(tasks)
