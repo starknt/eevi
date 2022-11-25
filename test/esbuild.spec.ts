fb35afd730ff2a1d160ca45018351c5cd6487d8d
starknt <1431880400@qq.com>
2022-11-25 16:05:35 +0800

    test(expose): add `transformPreload` function test case

---
+++ test/esbuild.spec.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from 'vitest'
+import { transformPreload } from '@eevi/elexpose'
+
+describe('transformPreload', () => {
+  it('transform `export const`', () => {
+    expect(transformPreload('export const a = 1').exposes, 'transfrom `export const` failed')
+      .toStrictEqual(['a'])
+
+    expect(transformPreload('const b = 2\nexport const a = b').exposes, 'transfrom `export const` failed')
+      .toStrictEqual(['a'])
+  })
+
+  it('transform `export function`', () => {
+    expect(transformPreload('export function func() {}').exposes, 'transfrom `export function` failed')
+      .toStrictEqual(['func'])
+
+    expect(transformPreload('const func = () => {}\nexport const exposeFunc = func').exposes, 'transfrom `export function` failed')
+      .toStrictEqual(['exposeFunc'])
+  })
+
+  it('transform `export  from \'./utils\'`', () => {
+    expect(transformPreload('export * as utils from \'./utils\'').exposes, 'export {} from` failed')
+      .toStrictEqual(['utils'])
+
+    expect(transformPreload('export { join, dirname, resolve } from \'./utils\'').exposes, 'transfrom `export {} from` failed')
+      .toStrictEqual(['join', 'dirname', 'resolve'])
+  })
+
+  it('transform `import` to `export`', () => {
+    expect(transformPreload(`
+      import sum from \'./utils\'
+
+      export const a = sum
+    `))
+      .toStrictEqual(['a'])
+  })
+})
